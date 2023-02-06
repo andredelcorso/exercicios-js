fbf765af9ff34ffd430f35c3694349bd0c4eeb7e
André Dal Corsi <andredalcorsi1@gmail.com>
2023-02-06 18:46:22 -0300

    Update ex003.js
    Substituindo cocatenação por Template String.

--- exercícios/ex003/ex003.js
+++ exercícios/ex003/ex003.js
@@ -1,7 +1,12 @@
+window.alert('Vamos calcular!')
 let n1 = Number(window.prompt('Digite um número'))
 let n2 = Number(window.prompt('Digite outro valor'))
 
 
 let soma = n1 + n2
+let mult = n1 * n2 
+let div = n1/n2 
 
-window.alert ('A soma dos dois números que você digitou é igual a: ' + soma)
+window.alert (`A soma entre ${n1} e ${n2} é igual a ${soma}`)
+window.alert (`A multiplicação entre ${n1} e ${n2} é igual a ${mult}`)
+window.alert (`A divisão entre ${n1} e ${n2} é igual a ${div}`)
