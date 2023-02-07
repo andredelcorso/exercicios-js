861ddeebfb470bd19a0a1d1bde2d18cdfdbea3a4
André Dal Corsi <andredalcorsi1@gmail.com>
2023-02-06 22:45:41 -0300

    Adicionando exercícios
    Adição de exercício 05 e correção de exercício 04.

--- exercícios/ex004/ex004.js
+++ exercícios/ex004/ex004.js
@@ -1,2 +1,4 @@
 let nome = window.prompt('Qual é o seu nome?')
-document.write(`O nome ${nome} tem ${nome.length} letras.`)
+document.writeln(`O nome ${nome} tem ${nome.length} letras.`)
+document.writeln(`Seu nome em maiúsculas fica assim: ${nome.toUpperCase()}`)
+document.writeln(`Seu nome em letras minúsculas fica assim: ${nome.toLowerCase ()}`)
