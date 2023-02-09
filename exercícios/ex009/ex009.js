886eb20a67eea299db53f71e0b49da3cd4edfad4
André Dal Corsi <andredalcorsi1@gmail.com>
2023-02-09 19:33:55 -0300

    Adição de exercícios
    Adicionando exerícios novos.

---
+++ exercícios/ex009/ex009.js
@@ -0,0 +1,9 @@
+let país = 'EUA'
+
+console.log(`Estou vivendo em: ${país}! Portanto eu sou: `)
+
+if (país == 'Brasil') {
+    console.log('Brasileiro')
+} else {
+    console.log('Estrangeiro!')
+}
