ea368062a6420451251a214ed2e29355ee652d68
André Dal Corsi <andredalcorsi1@gmail.com>
2023-02-17 02:33:22 -0300

    ADD Exercícios

---
+++ exercícios/Exercícios das Aulas/ex014/ex014.js
@@ -0,0 +1,28 @@
+// Aqui temos três exemplos de como usar estruturas de repetição. Lembrando que a última opção não é uma estrutura de repetição, pois não verifica nada. Apenas repete na força bruta. 
+
+let contador = 1 
+
+do {
+    console.log('Tudo bem?')
+    contador++
+} while (contador <= 6)
+
+
+/* while (contador <= 6) {
+    console.log('Tudo bem?')
+    contador ++ 
+} 
+*/
+
+/*
+
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+
+*/
---
+++ exercícios/Exercícios das Aulas/ex015/ex015.js
@@ -0,0 +1,5 @@
+console.log('Vai começar...')
+for (contador = 1; contador <= 3; contador++) {
+    console.log('Tudo bem?')
+}
+console.log('FIM!')
